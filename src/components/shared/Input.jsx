ddb4a88d7de26e0d356a1a0d5e1c262a06fd33d4
Mkmoise <mkmoise920@gmail.com>
2026-08-18 07:26:28 +0300

    create Input.jsx

---
+++ src/components/shared/Input.jsx
@@ -0,0 +1,27 @@
+function Input({
+  label,
+  name,
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  required = false,
+}) {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+
+      <input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+}
+
+export default Input;
