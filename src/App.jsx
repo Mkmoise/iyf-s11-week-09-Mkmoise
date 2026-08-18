f9d068df235f40267ac109eb02bfff867084d8b5
Mkmoise <mkmoise920@gmail.com>
2026-08-18 08:18:37 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./components/Layout/Layout";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
 import PostDetail from "./pages/PostDetail";
