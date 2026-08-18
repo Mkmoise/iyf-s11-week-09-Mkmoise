dc9e04df1c154946a8e676e9c3e80c34aa8b7d23
Mkmoise <141993247+Mkmoise@users.noreply.github.com>
2026-08-18 07:22:18 +0300

    Refactor Button component to accept props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
-function Button() {
+function Button({
+  children,
+  type = "button",
+  variant = "primary",
+  onClick,
+  disabled = false,
+}) {
   return (
-    <button>
-      Read More
+    <button
+      type={type}
+      className={`button button-${variant}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
     </button>
   );
 }
